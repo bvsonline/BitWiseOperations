67b4f377ed7f1a4e925ef870f8904e2dd707f7cd
Venkata B <bvsonline@gmail.com>
2019-04-18 19:59:31 -0700

    faster version of counting 1s in given value

--- BitWiseOperations/main.cpp
+++ BitWiseOperations/main.cpp
@@ -9,15 +9,24 @@
 #include <stdio.h>
 
 void CountNumberOfBits(unsigned int x, unsigned int *bits_1, unsigned int *bits_0);
+void CountNumberOf1s(unsigned int x, unsigned int *bits_1);
 
 int main()
 {
     
     unsigned int bit_1s = 0, bit_0s = 0;
-    
+
     CountNumberOfBits(123456, &bit_1s, &bit_0s);
-    
     printf("bit_1s: %d, bit_0s: %d \n", bit_1s, bit_0s);
+    
+    //bit_1s = 0;
+    //bit_0s = 0;
+    //CountNumberOfBits(568908, &bit_1s, &bit_0s);
+    //printf("bit_1s: %d, bit_0s: %d \n", bit_1s, bit_0s);
+    
+    bit_1s = 0;
+    CountNumberOf1s(123456, &bit_1s);
+    printf("bit_1s: %d\n", bit_1s);
     
     return 0;
 }
@@ -36,5 +45,13 @@ void CountNumberOfBits(unsigned int x, unsigned int *bits_1, unsigned int *bits_
        x >>= 1;
    }
 
+    return;
+}
+
+void CountNumberOf1s(unsigned int x, unsigned int *bits_1)
+{
+    for (*bits_1=0; x!=0; x&=x-1)
+        (*bits_1)++;
+    
     return;
 }
